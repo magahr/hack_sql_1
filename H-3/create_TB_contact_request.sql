f688d8231afa99580b7ed34a26e9d651388e2550
MagaDev <magahr123@gmail.com>
2024-08-19 20:45:25 -0400

    Making SQL1 H3

---
+++ H-3/create_TB_contact_request.sql
@@ -0,0 +1,10 @@
+
+create table contact_request(
+ id_email serial primary key,
+ id_country integer not null,
+ id_priority integer not null,
+ email varchar(100) not null,
+ name varchar (50) not null,
+ foreign key (id_country) references countries (id_country),
+ foreign key (id_priority) references priorities (id_priority)
+);
---
+++ H-3/create_TB_priorities.sql
@@ -0,0 +1,4 @@
+create table priorities(
+  id_priority serial primary key,
+  type_name varchar(50) not null  
+);
